9d9bcaefbdbe2a3ef86d3b8bae04681defc03bff
kukkook <lukkadeis22@gmail.com>
2023-01-27 20:07:35 +0700

    Update customers.dart

--- lib/customers.dart
+++ lib/customers.dart
@@ -11,57 +11,56 @@ class CustomerModel {
 }
 
 class CustomerPage extends StatelessWidget {
-  List<CustomerModel> customer = [
-    CustomerModel('customer/nia.png'),
-    CustomerModel('customer/viriya.png'),
-    CustomerModel('customer/royalTH.png'),
-    CustomerModel('customer/bitkub.png'),
-    CustomerModel('customer/bdms.png'),
-    CustomerModel('customer/boonrawd.png'),
-    CustomerModel('customer/brr.png'),
-    CustomerModel('customer/pat.png'),
-    CustomerModel('customer/advice.png'),
-    CustomerModel('customer/ichitan.png'),
-    CustomerModel('customer/singha.png'),
-    CustomerModel('customer/click.png'),
-    CustomerModel('customer/p-pat.png'),
-    CustomerModel('customer/rise-consulting.png'),
-    CustomerModel('customer/meng.png'),
-    CustomerModel('customer/singha-beer.png'),
-    CustomerModel('customer/daiichi.png'),
-    CustomerModel('customer/kdn.png'),
-    CustomerModel('customer/national-economic.png'),
-    CustomerModel('customer/intrarat.png'),
-    CustomerModel('customer/singha-park.png'),
-    CustomerModel('customer/muzik.png'),
-    CustomerModel('customer/santafe.png'),
-    CustomerModel('customer/ku.png'),
-    CustomerModel('customer/sbp.png'),
-    CustomerModel('customer/win.png')
+  List customer = [
+    'customer/nia.png',
+    'customer/viriya.png',
+    'customer/royalTH.png',
+    'customer/bitkub.png',
+    'customer/bdms.png',
+    'customer/boonrawd.png',
+    'customer/brr.png',
+    'customer/pat.png',
+    'customer/advice.png',
+    'customer/ichitan.png',
+    'customer/singha.png',
+    'customer/click.png',
+    'customer/p-pat.png',
+    'customer/rise-consulting.png',
+    'customer/meng.png',
+    'customer/singha-beer.png',
+    'customer/daiichi.png',
+    'customer/kdn.png',
+    'customer/national-economic.png',
+    'customer/intrarat.png',
+    'customer/singha-park.png',
+    'customer/muzik.png',
+    'customer/santafe.png',
+    'customer/ku.png',
+    'customer/sbp.png',
+    'customer/win.png',
   ];
 
-  bool isSmallScreen = false;
-  bool isLargeScreen = false;
-  double _opacity = 0;
-  bool _isNavMenuVisible = false;
+  // bool isSmallScreen = false;
+  // bool isLargeScreen = false;
+  // double _opacity = 0;
+  // bool _isNavMenuVisible = false;
 
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    if (screenSize.width > 1000) {
-      isLargeScreen = true;
-    } else {
-      isSmallScreen = true;
-    }
+    // if (screenSize.width > 1000) {
+    //   isLargeScreen = true;
+
+    // }
 
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 228, 236, 248),
+      backgroundColor: Colors.white,
 
       body: SingleChildScrollView(
         child: Center(
           child: Container(
             width: 1000,
-            color: Color.fromARGB(255, 228, 236, 248),
+            color: Colors.white,
             margin: EdgeInsets.all(00.0),
             child: Padding(
               padding: const EdgeInsets.all(20.0),
@@ -70,56 +69,70 @@ class CustomerPage extends StatelessWidget {
                   children: [
                     Text(
                       'ลูกค้าคนสำคัญของเรา',
-                      style: GoogleFonts.ibmPlexSans(
+                      style: GoogleFonts.nunito(
                         fontWeight: FontWeight.w700,
                         color: Color.fromARGB(255, 24, 84, 110),
                         fontSize: 35,
                       ),
                     ),
-                    GridView.builder(
-                        shrinkWrap: true,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: screenSize.width > 1000 ? 5 : 3,
-                          crossAxisSpacing: 20,
-                          mainAxisSpacing: 20,
+                    Column(
+                      children: [
+                        Text(
+                          'ด้วยประสบการณ์ Software Solutions ครบวงจร เราได้รับความไว้วางใจให้ดูแลธุรกิจทั้งองค์กรภาครัฐ องค์กรเอกชน ตลอดจนธุรกิจขนาดเล็ก หรือ SME ',
+                          style: GoogleFonts.ibmPlexSansThai(
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromARGB(255, 24, 84, 110),
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        itemCount: customer.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            // decoration: BoxDecoration(
-                            //   borderRadius: BorderRadius.circular(20),
-                            //   color: Color.fromARGB(255, 178, 205, 244),
-                            // ),
-                            child: new Material(
-                              child: new InkWell(
-                                borderRadius: BorderRadius.circular(20),
-                                onTap: () {},
-                                child: new Container(
-                                  width: 180,
-                                  height: 180,
-                                  child: Center(
-                                    child: Container(
-                                      margin: EdgeInsets.all(15),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          SizedBox(
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        GridView.builder(
+                            shrinkWrap: true,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: screenSize.width > 1000 ? 5 : 3,
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20,
+                            ),
+                            itemCount: customer.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                // decoration: BoxDecoration(
+                                //   borderRadius: BorderRadius.circular(20),
+                                //   color: Color.fromARGB(255, 178, 205, 244),
+                                // ),
+                                child: new Material(
+                                  child: new Container(
+                                    width: 180,
+                                    height: 180,
+                                    child: Center(
+                                      child: Container(
+                                        margin: EdgeInsets.all(15),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            SizedBox(
                                               width: 100,
                                               height: 100,
-                                              child: Image.asset(
-                                                'assets/${customer[index].imageUrl}',
-                                              )),
-                                        ],
+                                              child:
+                                                  Image.asset(customer[index]),
+                                            )
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
+                                  color: Colors.transparent,
                                 ),
-                              ),
-                              color: Colors.transparent,
-                            ),
-                          );
-                        }),
+                              );
+                            }),
+                      ],
+                    ),
                   ],
                 ),
               ),
