a4166ebd69339a3569ebfb7a96bb5cab532f9439
kukkook <lukkadeis22@gmail.com>
2023-01-27 11:21:22 +0700

    nav

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -18,58 +18,13 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _MyHomePageState();
 }
 
-// final List<String> listHome = <String>['h1', 'h2', 'h3', 'h4'];
-// final List<String> listAbout = <String>['ab1', 'ab2', 'ab3', 'ab4'];
-// final List<String> listServices = <String>['sv1', 'sv2', 'sv3', 'sv4'];
-// final List<String> listBlogs = <String>['b1', 'b2', 'b3', 'b4'];
-// final List<String> listContact = <String>['ct1', 'ct2', 'ct3', 'ct4'];
+final List<String> listHome = <String>['h1', 'h2', 'h3', 'h4'];
+final List<String> listAbout = <String>['ab1', 'ab2', 'ab3', 'ab4'];
+final List<String> listServices = <String>['sv1', 'sv2', 'sv3', 'sv4'];
+final List<String> listBlogs = <String>['b1', 'b2', 'b3', 'b4'];
+final List<String> listContact = <String>['ct1', 'ct2', 'ct3', 'ct4'];
 
 class _MyHomePageState extends State<HomePage> {
-  // List title = [
-  //   " PD Management",
-  //   "DPIA & Risk Management",
-  //   "Cookie Consent",
-  //   "Consent Management",
-  //   "Data Subject Right Management",
-  //   "Data Breach",
-  //   "Audit&Gap Management",
-  //   "Policy&Notices Management",
-  //   "Data Deiscover",
-  //   "Executive Support System",
-  //   "Legitimate Interest Assessment",
-  //   "System Setting Management",
-  // ];
-
-  // List desc = [
-  //   " จัดการบันทึกรายการข้อมูลส่วนบุคคลและการไหลของข้อมูล",
-  //   "การบริหารความเสี่ยงและผลกระทบจากข้อมูลส่วนบุคคล",
-  //   "การขอความยินยอมจากเจ้าของข้อมูลและการบริหารจัดการคุกกี้",
-  //   "การบริหารจัดการให้ความยินยอม",
-  //   "บริหารการขอใช้สิทธิ์จากเจ้าของข้อมูล",
-  //   "บริหารกรณีเกิดข้อมูลรั่วไหล",
-  //   "บริหารงานตรจสอบและการทำ GAP Analysis",
-  //   "บริหารการสื่อสารนโยบายและประกาศความเป็นส่วนตัว",
-  //   "ค้นหาข้อมูลส่วนตัวในระบบสารสนเทศ",
-  //   "บริหารงานและจัดการรายงาน สำหรับผู้บริหาร",
-  //   "การประเมินการนำฐานกฏหมายมาใช้",
-  //   "ส่วนการตั้งค่า ส่วนส่งเสริมที่ถูกพัฒนาขึ้นมาเพื่อให้ผู้ดูแลระบบจัดการกับข้อมูลต่างๆ",
-  // ];
-
-  // List img = [
-  //   "folder.png",
-  //   "risk.png",
-  //   "Cookie.png",
-  //   "Consent.png",
-  //   "data.png",
-  //   "breach.png",
-  //   "audit.png",
-  //   "Policy.png",
-  //   "discovery.png",
-  //   "executive.png",
-  //   "legi.png",
-  //   "system.png",
-  // ];
-
   List<FeatureModel> features = [
     FeatureModel(
         "PD Management",
@@ -120,6 +75,7 @@ class _MyHomePageState extends State<HomePage> {
 
   bool isSmallScreen = false;
   bool isLargeScreen = false;
+  bool isMedScreen = false;
   double _opacity = 0;
   bool _isNavMenuVisible = false;
 
@@ -133,8 +89,10 @@ class _MyHomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // var deviceData = MediaQuery.of(context);
     var screenSize = MediaQuery.of(context).size;
-    if (screenSize.width > 700) {
+    if (screenSize.width > 1000) {
       isLargeScreen = true;
+    // } else if (screenSize.width <= 1000 && screenSize.width > 700) {
+    //   isMedScreen = true;
     } else {
       isSmallScreen = true;
     }
@@ -142,114 +100,133 @@ class _MyHomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 228, 236, 248),
       appBar: PreferredSize(
-        preferredSize: Size(screenSize.width, 60),
-        child: Column(
-          children: [
-            TopBarContents(_opacity),
-          ],
-        ),
+        preferredSize: Size(screenSize.width, 80),
+        child: Navbar(_opacity),
       ),
-      body: Center(
-        child: Container(
-          width: 1000,
-          color: Color.fromARGB(255, 228, 236, 248),
-          margin: EdgeInsets.all(00.0),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Center(
-              child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: screenSize.width > 700 ? 3 : 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                  ),
-                  itemCount: features.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      // decoration: BoxDecoration(
-                      //   borderRadius: BorderRadius.circular(20),
-                      //   color: Color.fromARGB(255, 178, 205, 244),
-                      // ),
-                      child: new Material(
-                        child: new InkWell(
-                          onTap: () {},
-                          child: new Container(
-                            width: 350,
-                            height: 350,
-                            child: Center(
-                              child: Container(
-                                margin: EdgeInsets.all(15),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    SizedBox(
-                                        width: 50,
-                                        height: 50,
-                                        child: Image.asset(
-                                          'assets/${features[index].imageUrl}',
-                                        )),
-                                    Text(
-                                      '${features[index].title}',
-                                      style: GoogleFonts.poppins(
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 16,
-                                          color:
-                                              Color.fromARGB(255, 16, 24, 40)),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    Text(
-                                      '${features[index].description}',
-                                      style: GoogleFonts.ibmPlexSansThai(
-                                        color:
-                                            Color.fromARGB(255, 102, 112, 133),
-                                        fontSize: 13,
-                                      ),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    Container(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: 1000,
+            color: Color.fromARGB(255, 228, 236, 248),
+            margin: EdgeInsets.all(00.0),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Key Features',
+                      style: GoogleFonts.ibmPlexSans(
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromARGB(255, 24, 84, 110),
+                        fontSize: 35,
+                      ),
+                    ),
+                    GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: screenSize.width > 1000 ? 3 : 1,
+                          crossAxisSpacing: 20,
+                          mainAxisSpacing: 20,
+                        ),
+                        itemCount: features.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            // decoration: BoxDecoration(
+                            //   borderRadius: BorderRadius.circular(20),
+                            //   color: Color.fromARGB(255, 178, 205, 244),
+                            // ),
+                            child: new Material(
+                              child: new InkWell(
+                                onTap: () {},
+                                child: new Container(
+                                  width: 300,
+                                  height: 300,
+                                  child: Center(
+                                    child: Container(
+                                      margin: EdgeInsets.all(15),
                                       child: Column(
-                                        children: [
-                                          TextButton(
-                                            onPressed: () {},
-                                            child: Row(
-                                              mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          SizedBox(
+                                              width: 100,
+                                              height: 100,
+                                              child: Image.asset(
+                                                'assets/${features[index].imageUrl}',
+                                              )),
+                                          Text(
+                                            '${features[index].title}',
+                                            style: GoogleFonts.poppins(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 22,
+                                                color: Color.fromARGB(
+                                                    255, 16, 24, 40)),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                          Text(
+                                            '${features[index].description}',
+                                            style: GoogleFonts.ibmPlexSansThai(
+                                              color: Color.fromARGB(
+                                                  255, 102, 112, 133),
+                                              fontSize: 16,
+                                            ),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                          Container(
+                                            child: Column(
                                               children: [
-                                                Text(
-                                                  'more ',
-                                                  style: GoogleFonts.inter(
-                                                      fontSize: 14,
-                                                      color: Color.fromARGB(
-                                                          255, 57, 129, 237)),
-                                                ), // <-- Text
+                                                TextButton(
+                                                  onPressed: () {},
+                                                  child: Row(
+                                                    mainAxisSize:
+                                                        MainAxisSize.min,
+                                                    children: [
+                                                      Text(
+                                                        'more ',
+                                                        style:
+                                                            GoogleFonts.inter(
+                                                                fontSize: 18,
+                                                                color: Color
+                                                                    .fromARGB(
+                                                                        255,
+                                                                        57,
+                                                                        129,
+                                                                        237)),
+                                                      ), // <-- Text
 
-                                                Icon(
-                                                  // <-- Icon
-                                                  Icons.arrow_forward_sharp,
-                                                  color: Color.fromARGB(
-                                                      255, 57, 129, 237),
-                                                  size: 14.0,
+                                                      Icon(
+                                                        // <-- Icon
+                                                        Icons
+                                                            .arrow_forward_sharp,
+                                                        color: Color.fromARGB(
+                                                            255, 57, 129, 237),
+                                                        size: 18.0,
+                                                      ),
+                                                    ],
+                                                  ),
                                                 ),
                                               ],
                                             ),
-                                          ),
+                                          )
                                         ],
                                       ),
-                                    )
-                                  ],
+                                    ),
+                                  ),
                                 ),
                               ),
+                              color: Colors.transparent,
                             ),
-                          ),
-                        ),
-                        color: Colors.transparent,
-                      ),
-                    );
-                  }),
+                          );
+                        }),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
       ),
-      // bottomNavigationBar: MyBottomNavigationBar()
+      // drawer: NavBar(),
     );
   }
 }
--- lib/navbar.dart
+++ lib/navbar.dart
@@ -3,246 +3,126 @@ import 'HomePage.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TopBarContents extends StatefulWidget {
+class Navbar extends StatefulWidget {
   final double opacity;
 
-  TopBarContents(this.opacity);
-
+  Navbar(this.opacity);
   @override
-  _TopBarContentsState createState() => _TopBarContentsState();
+  _Navbar createState() => _Navbar();
 }
 
-final List<String> listHome = <String>['h1', 'h2', 'h3', 'h4'];
-final List<String> listAbout = <String>['ab1', 'ab2', 'ab3', 'ab4'];
-final List<String> listServices = <String>['sv1', 'sv2', 'sv3', 'sv4'];
-final List<String> listBlogs = <String>['b1', 'b2', 'b3', 'b4'];
-final List<String> listContact = <String>['ct1', 'ct2', 'ct3', 'ct4'];
+class _Navbar extends State<Navbar> {
+  final List<String> listHome = <String>['h1', 'h2', 'h3', 'h4'];
+  final List<String> listAbout = <String>['ab1', 'ab2', 'ab3', 'ab4'];
+  final List<String> listServices = <String>['sv1', 'sv2', 'sv3', 'sv4'];
+  final List<String> listBlogs = <String>['b1', 'b2', 'b3', 'b4'];
+  final List<String> listContact = <String>['ct1', 'ct2', 'ct3', 'ct4'];
 
-class _TopBarContentsState extends State<TopBarContents> {
   String? HomeValue;
   String? AboutValue;
   String? ServicesValue;
   String? BlogsValue;
   String? ContactValue;
 
+  bool _isProcessing = false;
+
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-
+    final screenSize = MediaQuery.of(context).size;
     return PreferredSize(
       preferredSize: Size(screenSize.width, 1000),
       child: Container(
-        child: AppBar(
-          title: Text('Hello'),
-          leading: Image.asset(
-            'logo.png',
-            // height: 250,
-            // width: 200,
+        color: Color.fromARGB(255, 52, 144, 206),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('logo.png',
+              height: 50,),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    DropdownButtonHideUnderline(
+                      child: DropdownButton2(
+                        hint: Text(
+                          'Home',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        icon: Transform.translate(
+                          offset: Offset(-14, 0),
+                          child: Icon(Icons.expand_more_outlined,
+                          color: Colors.white,),
+                        ),
+                        items: listHome
+                            .map((item) => DropdownMenuItem<String>(
+                                  value: item,
+                                  child: Text(
+                                    item,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ))
+                            .toList(),
+                        value: HomeValue,
+                        onChanged: (value) {
+                          setState(() {
+                            HomeValue = value as String;
+                          });
+                        },
+                        buttonHeight: 40,
+                        buttonWidth: 80,
+                        itemHeight: 40,
+                      ),
+                    ),
+                    DropdownButtonHideUnderline(
+                      child: DropdownButton2(
+                        hint: Text(
+                          'About',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        icon: Transform.translate(
+                          offset: Offset(-14, 0),
+                          child: Icon(Icons.expand_more_outlined,
+                          color: Colors.white,),
+                        ),
+                        items: listAbout
+                            .map((item) => DropdownMenuItem<String>(
+                                  value: item,
+                                  child: Text(
+                                    item,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ))
+                            .toList(),
+                        value:AboutValue,
+                        onChanged: (value) {
+                          setState(() {
+                            AboutValue = value as String;
+                          });
+                        },
+                        buttonHeight: 40,
+                        buttonWidth:80,
+                        itemHeight: 40,
+                      ),
+                    ),
+                    
+                    
+                  ],
+                ),
+              ),
+            ],
           ),
-          
-          backgroundColor: Color.fromARGB(255, 52, 144, 206),
-          actions: <Widget>[
-            DropdownButtonHideUnderline(
-              child: DropdownButton2(
-                icon: Transform.translate(
-                  offset: (Offset(-15, 0)),
-                  child: const Icon(
-                    Icons.expand_more,
-                    color: Colors.white,
-                  ),
-                ),
-                hint: Text(
-                  'Home',
-                  style: GoogleFonts.ibmPlexSans(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                items: listHome
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Color.fromARGB(255, 87, 87, 87),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-                value: HomeValue,
-                onChanged: (value) {
-                  setState(() {
-                    HomeValue = value as String;
-                  });
-                },
-                buttonHeight: 20,
-                buttonWidth: 80,
-                itemHeight: 40,
-              ),
-            ),
-            DropdownButtonHideUnderline(
-              child: DropdownButton2(
-                icon: Transform.translate(
-                  offset: (Offset(-13, 0)),
-                  child: const Icon(
-                    Icons.expand_more,
-                    color: Colors.white,
-                  ),
-                ),
-                hint: Text(
-                  'About Us',
-                  style: GoogleFonts.ibmPlexSans(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                items: listAbout
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Color.fromARGB(255, 87, 87, 87),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-                value: AboutValue,
-                onChanged: (value) {
-                  setState(() {
-                    AboutValue = value as String;
-                  });
-                },
-                buttonHeight: 20,
-                buttonWidth: 100,
-                itemHeight: 40,
-              ),
-            ),
-            DropdownButtonHideUnderline(
-              child: DropdownButton2(
-                icon: Transform.translate(
-                  offset: (Offset(-10, 0)),
-                  child: const Icon(
-                    Icons.expand_more,
-                    color: Colors.white,
-                  ),
-                ),
-                hint: Text(
-                  'Our Services',
-                  style: GoogleFonts.ibmPlexSans(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                items: listServices
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Color.fromARGB(255, 87, 87, 87),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-                value: ServicesValue,
-                onChanged: (value) {
-                  setState(() {
-                    ServicesValue = value as String;
-                  });
-                },
-                buttonHeight: 20,
-                buttonWidth: 120,
-                itemHeight: 40,
-              ),
-            ),
-            DropdownButtonHideUnderline(
-              child: DropdownButton2(
-                icon: Transform.translate(
-                  offset: (Offset(-8, 0)),
-                  child: const Icon(
-                    Icons.expand_more,
-                    color: Colors.white,
-                  ),
-                ),
-                hint: Text(
-                  'Blogs',
-                  style: GoogleFonts.ibmPlexSans(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                items: listBlogs
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Color.fromARGB(255, 87, 87, 87),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-                value: BlogsValue,
-                onChanged: (value) {
-                  setState(() {
-                    BlogsValue = value as String;
-                  });
-                },
-                buttonHeight: 20,
-                buttonWidth: 70,
-                itemHeight: 40,
-              ),
-            ),
-            DropdownButtonHideUnderline(
-              child: DropdownButton2(
-                icon: Transform.translate(
-                  offset: (Offset(-17, 0)),
-                  child: const Icon(
-                    Icons.expand_more,
-                    color: Colors.white,
-                  ),
-                ),
-                hint: Text(
-                  'Contact Us',
-                  style: GoogleFonts.ibmPlexSans(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                items: listContact
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Color.fromARGB(255, 87, 87, 87),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-                value: ContactValue,
-                onChanged: (value) {
-                  setState(() {
-                    ContactValue = value as String;
-                  });
-                },
-                buttonHeight: 20,
-                buttonWidth: 120,
-                itemHeight: 40,
-              ),
-            ),
-          ],
         ),
       ),
     );
