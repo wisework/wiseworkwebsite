8a4941571fe21765101aec9ab9cda816124968fb
Bearri <122964343+Bearri@users.noreply.github.com>
2023-02-08 01:14:45 +0700

    Update carousel.dart

--- lib/home/carousel.dart
+++ lib/home/carousel.dart
@@ -57,87 +57,54 @@ class _DestinationCarouselState extends State<CarouselPage> {
     var screenSize = MediaQuery.of(context).size;
     var imageSliders = generateImageTiles(screenSize);
     final width = MediaQuery.of(context).size.width;
-    return Stack(
-      children: [
-        Column(
-          children: [
-            CarouselSlider(
-              items: imageSliders,
-              options: CarouselOptions(
-                  scrollPhysics:
-                      //  Responsive.isMobile(context)
-                      //     ?
-                      PageScrollPhysics(),
-                  // : NeverScrollableScrollPhysics(),
-                  // enlargeCenterPage: true,
-                  aspectRatio: 28 / 8,
-                  autoPlay: true,
-                  viewportFraction: 1,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                      for (int i = 0; i < imageSliders.length; i++) {
-                        if (i == index) {
-                          _isSelected[i] = true;
-                        } else {
-                          _isSelected[i] = false;
-                        }
-                      }
-                    });
-                  }),
-              carouselController: _controller,
-            ),
-            SizedBox(
-                height: Responsive.isDesktop(context)
-                    ? 90
-                    : Responsive.isTablet(context)
-                        ? 40
-                        : 20),
-            Container(
-              height: 10,
-              color: Colors.black,
-              child: CarouselIndicator(
-                width: 10,
-                height: 4,
-                color: Colors.white,
-                activeColor: Color.fromARGB(255, 0, 195, 255),
-                // animationDuration: _current,
-                count: imageSliders.length,
-                index: _current,
+    return Container(
+      width: 1440,
+      color: Color.fromARGB(255, 232, 242, 254),
+      child: Column(
+        children: [
+          SizedBox(height: 83),
+          Stack(
+            children: [
+              Column(
+                children: [
+                  CarouselSlider(
+                    items: imageSliders,
+                    options: CarouselOptions(
+                        scrollPhysics:
+                            //  Responsive.isMobile(context)
+                            //     ?
+                            PageScrollPhysics(),
+                        // : NeverScrollableScrollPhysics(),
+                        // enlargeCenterPage: true,
+                        aspectRatio: 28 / 11,
+                        autoPlay: true,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _current = index;
+                            for (int i = 0; i < imageSliders.length; i++) {
+                              if (i == index) {
+                                _isSelected[i] = true;
+                              } else {
+                                _isSelected[i] = false;
+                              }
+                            }
+                          });
+                        }),
+                    carouselController: _controller,
+                  ),
+                  SizedBox(
+                      height: Responsive.isDesktop(context)
+                          ? 97
+                          : Responsive.isTablet(context)
+                              ? 40
+                              : 20),
+                ],
               ),
-            ),
-            SizedBox(height: 10)
-          ],
-        ),
-
-        // AspectRatio(
-        //   aspectRatio: 28 / 10,
-        //   child: Center(
-        //     child: Text(
-        //       places[_current],
-        //       style: TextStyle(
-        //         letterSpacing: 8,
-        //         fontFamily: 'Electrolize',
-        //         fontSize: screenSize.width / 25,
-        //         color: Colors.white,
-        //       ),
-        //     ),
-        //   ),
-        // ),
-        // Responsive.isMobile(context)
-        //     ? Container()
-        //     :
-        AspectRatio(
-          aspectRatio: 28 / 8,
-          child: Center(
-            heightFactor: 1,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  left: screenSize.width / 8,
-                  right: screenSize.width / 8,
-                ),
+              Positioned(
+                bottom: 50,
+                left: 410,
+                right: 403,
                 child: SizedBox(
                   width: Responsive.isDesktop(context)
                       ? 628
@@ -168,85 +135,129 @@ class _DestinationCarouselState extends State<CarouselPage> {
                             fontWeight: FontWeight.w500),
                       )),
                 ),
-
-                // child: Card(
-                //   elevation: 5,
-                //   child: Padding(
-                //     padding: EdgeInsets.only(
-                //       top: screenSize.height / 50,
-                //       bottom: screenSize.height / 50,
-                //     ),
-                //     child: Row(
-                //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                //       children: [
-                //         for (int i = 0; i < places.length; i++)
-                //           Column(
-                //             mainAxisSize: MainAxisSize.min,
-                //             children: [
-                //               InkWell(
-                //                 splashColor: Colors.transparent,
-                //                 hoverColor: Colors.transparent,
-                //                 onHover: (value) {
-                //                   setState(() {
-                //                     value
-                //                         ? _isHovering[i] = true
-                //                         : _isHovering[i] = false;
-                //                   });
-                //                 },
-                //                 onTap: () {
-                //                   _controller.animateToPage(i);
-                //                 },
-                //                 child: Padding(
-                //                   padding: EdgeInsets.only(
-                //                       top: screenSize.height / 80,
-                //                       bottom: screenSize.height / 90),
-                //                   child: Text(
-                //                     places[i],
-                //                     style: TextStyle(
-                //                       color: _isHovering[i]
-                //                           ? Theme.of(context)
-                //                               .primaryTextTheme
-                //                               .button!
-                //                               .decorationColor
-                //                           : Theme.of(context)
-                //                               .primaryTextTheme
-                //                               .button!
-                //                               .color,
-                //                     ),
-                //                   ),
-                //                 ),
-                //               ),
-                //               Visibility(
-                //                 maintainSize: true,
-                //                 maintainAnimation: true,
-                //                 maintainState: true,
-                //                 visible: _isSelected[i],
-                //                 child: AnimatedOpacity(
-                //                   duration: Duration(milliseconds: 400),
-                //                   opacity: _isSelected[i] ? 1 : 0,
-                //                   child: Container(
-                //                     height: 5,
-                //                     decoration: BoxDecoration(
-                //                       color: Colors.blueGrey,
-                //                       borderRadius: BorderRadius.all(
-                //                         Radius.circular(10),
-                //                       ),
-                //                     ),
-                //                     width: screenSize.width / 10,
-                //                   ),
-                //                 ),
-                //               )
-                //             ],
-                //           ),
-                //       ],
-                //     ),
-                //   ),
-                // ),
               ),
-            ),
+
+              // AspectRatio(
+              //   aspectRatio: 28 / 10,
+              //   child: Center(
+              //     child: Text(
+              //       places[_current],
+              //       style: TextStyle(
+              //         letterSpacing: 8,
+              //         fontFamily: 'Electrolize',
+              //         fontSize: screenSize.width / 25,
+              //         color: Colors.white,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // Responsive.isMobile(context)
+              //     ? Container()
+              //     :
+
+              // AspectRatio(
+              //   aspectRatio: 28 / 8,
+              //   child: Center(
+              //     heightFactor: 1,
+              //     child: Align(
+              //       alignment: Alignment.bottomCenter,
+              //       child: Padding(
+              //         padding: EdgeInsets.only(
+              //           left: screenSize.width / 8,
+              //           right: screenSize.width / 8,
+              //         ),
+
+              // child: Card(
+              //   elevation: 5,
+              //   child: Padding(
+              //     padding: EdgeInsets.only(
+              //       top: screenSize.height / 50,
+              //       bottom: screenSize.height / 50,
+              //     ),
+              //     child: Row(
+              //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //       children: [
+              //         for (int i = 0; i < places.length; i++)
+              //           Column(
+              //             mainAxisSize: MainAxisSize.min,
+              //             children: [
+              //               InkWell(
+              //                 splashColor: Colors.transparent,
+              //                 hoverColor: Colors.transparent,
+              //                 onHover: (value) {
+              //                   setState(() {
+              //                     value
+              //                         ? _isHovering[i] = true
+              //                         : _isHovering[i] = false;
+              //                   });
+              //                 },
+              //                 onTap: () {
+              //                   _controller.animateToPage(i);
+              //                 },
+              //                 child: Padding(
+              //                   padding: EdgeInsets.only(
+              //                       top: screenSize.height / 80,
+              //                       bottom: screenSize.height / 90),
+              //                   child: Text(
+              //                     places[i],
+              //                     style: TextStyle(
+              //                       color: _isHovering[i]
+              //                           ? Theme.of(context)
+              //                               .primaryTextTheme
+              //                               .button!
+              //                               .decorationColor
+              //                           : Theme.of(context)
+              //                               .primaryTextTheme
+              //                               .button!
+              //                               .color,
+              //                     ),
+              //                   ),
+              //                 ),
+              //               ),
+              //               Visibility(
+              //                 maintainSize: true,
+              //                 maintainAnimation: true,
+              //                 maintainState: true,
+              //                 visible: _isSelected[i],
+              //                 child: AnimatedOpacity(
+              //                   duration: Duration(milliseconds: 400),
+              //                   opacity: _isSelected[i] ? 1 : 0,
+              //                   child: Container(
+              //                     height: 5,
+              //                     decoration: BoxDecoration(
+              //                       color: Colors.blueGrey,
+              //                       borderRadius: BorderRadius.all(
+              //                         Radius.circular(10),
+              //                       ),
+              //                     ),
+              //                     width: screenSize.width / 10,
+              //                   ),
+              //                 ),
+              //               )
+              //             ],
+              //           ),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+            ],
           ),
-        ),
-      ],
+          CarouselIndicator(
+            width: 10,
+            height: 4,
+            color: Color.fromARGB(64, 0, 0, 0),
+            activeColor: Color.fromARGB(255, 0, 0, 0),
+            // animationDuration: _current,
+            count: imageSliders.length,
+            index: _current,
+          ),
+          SizedBox(height: 61)
+        ],
+      ),
     );
   }
 }
