cf582ed66d71dabfe41cf6e4735fddba9b9a4d4b
Bearri <122964343+Bearri@users.noreply.github.com>
2023-02-13 01:41:48 +0700

    pk

--- lib/model/package.dart
+++ lib/model/package.dart
@@ -3,18 +3,18 @@
 class Package {
   final String title;
   final List<String> children;
+  final bool position;
   Package({
     required this.title,
     required this.children,
+    required this.position,
   });
 
-  Package copyWith({
-    String? title,
-    List<String>? children,
-  }) {
+  Package copyWith({String? title, List<String>? children, bool? position}) {
     return Package(
       title: title ?? this.title,
       children: children ?? this.children,
+      position: position ?? this.position,
     );
   }
 }
