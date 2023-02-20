9724d55aaa3520f9857ab4f5e07c3880257dca96
kukkook <lukkadeis22@gmail.com>
2023-02-21 00:51:28 +0700

    advise-tl&mb

--- lib/about/advise.dart
+++ lib/about/advise.dart
@@ -11,137 +11,240 @@ class _AdviseState extends State<AdvisePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      width: 1440,
-      height: 556,
-      color: Color.fromARGB(255, 5, 45, 97),
-      child: Stack(
-        children: [
-          Container(
-            width: 1440,
-            height: 556,
-            color: Color.fromARGB(255, 5, 45, 97),
-          ),
-          new Positioned(
-              bottom: 480,
-              child: Container(
-                width: 390,
-                height: 390,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.transparent,
-                    border: Border.all(
-                        color: Color.fromARGB(62, 57, 129, 237), width: 35)),
-              )),
-          new Positioned(
-              bottom: 420,
-              right: 980,
-              child: Container(
-                width: 525,
-                height: 525,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.transparent,
-                    border: Border.all(
-                        color: Color.fromARGB(62, 57, 129, 237), width: 35)),
-              )),
-          new Positioned(
-              bottom: 360,
-              right: 910,
-              child: Container(
-                width: 663,
-                height: 663,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.transparent,
-                    border: Border.all(
-                        color: Color.fromARGB(62, 57, 129, 237), width: 35)),
-              )),
-          new Positioned(
-              bottom: 100,
-              left: 1380,
-              child: Container(
-                width: 390,
-                height: 390,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.transparent,
-                    border: Border.all(
-                        color: Color.fromARGB(62, 57, 129, 237), width: 35)),
-              )),
-          new Positioned(
-              bottom: 40,
-              left: 1310,
-              child: Container(
-                width: 525,
-                height: 525,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.transparent,
-                    border: Border.all(
-                        color: Color.fromARGB(62, 57, 129, 237), width: 35)),
-              )),
-          new Positioned(
-            top: 166.5,
-            left: 118,
-            child: Container(
-              width: 540,
-              height: 65,
-              // color: Colors.amber,
-              child: Text(
-                "พร้อมวางแผนให้ธุรกิจคุณ!",
-                style: GoogleFonts.ibmPlexSans(
-                    color: Colors.white,
-                    fontSize: 48,
-                    fontWeight: FontWeight.w600),
-                textAlign: TextAlign.start,
-              ),
-            ),
-          ),
-          new Positioned(
-              top: 240,
-              left: 118,
-              child: Text(
-                "ให้องค์กรของคุณ วางแผนและจัดการกับ DATA หัวใจสำคัญของธุรกิจ\nได้ถูกต้องตามกฎหมาย ปรึกษาเรา #TeamWiseWork",
-                style: GoogleFonts.ibmPlexSansThai(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white),
-                textAlign: TextAlign.start,
-              )),
-          new Positioned(
-            top: 47,
-            left: 745,
-            child: SizedBox(
-                width: 506,
-                height: 435,
-                child: Image.asset("/about/advise/comp.png")),
-          ),
-          new Positioned(
-            top: 375,
-            left: 118,
-            child: SizedBox(
-              width: 193,
-              height: 40,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 75, 195, 211),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)),
+        child: Responsive.isDesktop(context)
+            ? Container(
+                width: 1440,
+                height: 556,
+                color: Color.fromARGB(255, 5, 45, 97),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 1440,
+                      height: 556,
+                      color: Color.fromARGB(255, 5, 45, 97),
+                    ),
+                    new Positioned(
+                        bottom: 480,
+                        child: Container(
+                          width: 390,
+                          height: 390,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: Color.fromARGB(62, 57, 129, 237),
+                                  width: 35)),
+                        )),
+                    new Positioned(
+                        bottom: 420,
+                        right: 980,
+                        child: Container(
+                          width: 525,
+                          height: 525,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: Color.fromARGB(62, 57, 129, 237),
+                                  width: 35)),
+                        )),
+                    new Positioned(
+                        bottom: 360,
+                        right: 910,
+                        child: Container(
+                          width: 663,
+                          height: 663,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: Color.fromARGB(62, 57, 129, 237),
+                                  width: 35)),
+                        )),
+                    new Positioned(
+                        bottom: 100,
+                        left: 1380,
+                        child: Container(
+                          width: 390,
+                          height: 390,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: Color.fromARGB(62, 57, 129, 237),
+                                  width: 35)),
+                        )),
+                    new Positioned(
+                        bottom: 40,
+                        left: 1310,
+                        child: Container(
+                          width: 525,
+                          height: 525,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                  color: Color.fromARGB(62, 57, 129, 237),
+                                  width: 35)),
+                        )),
+                    new Positioned(
+                      top: 166.5,
+                      left: 118,
+                      child: Container(
+                        width: 540,
+                        height: 65,
+                        // color: Colors.amber,
+                        child: Text(
+                          "พร้อมวางแผนให้ธุรกิจคุณ!",
+                          style: GoogleFonts.ibmPlexSans(
+                              color: Colors.white,
+                              fontSize: 48,
+                              fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ),
+                    new Positioned(
+                        top: 240,
+                        left: 118,
+                        child: Text(
+                          "ให้องค์กรของคุณ วางแผนและจัดการกับ DATA หัวใจสำคัญของธุรกิจ\nได้ถูกต้องตามกฎหมาย ปรึกษาเรา #TeamWiseWork",
+                          style: GoogleFonts.ibmPlexSansThai(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white),
+                          textAlign: TextAlign.start,
+                        )),
+                    new Positioned(
+                      top: 47,
+                      left: 745,
+                      child: SizedBox(
+                          width: 506,
+                          height: 435,
+                          child: Image.asset("/about/advise/comp.png")),
+                    ),
+                    new Positioned(
+                      top: 375,
+                      left: 118,
+                      child: SizedBox(
+                        width: 193,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 75, 195, 211),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                          ),
+                          child: Text(
+                            'ติดต่อเรา',
+                            style: GoogleFonts.ibmPlexSansThai(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                child: Text(
-                  'ติดต่อเรา',
-                  style: GoogleFonts.ibmPlexSansThai(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white),
+              )
+            : Container(
+                width: Responsive.isTablet(context) ? 768 : 375,
+                height: Responsive.isTablet(context) ? 385 : 265,
+                color: Color.fromARGB(255, 5, 45, 97),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: Responsive.isTablet(context) ? 768 : 375,
+                      height: Responsive.isTablet(context) ? 385 : 265,
+                      color: Color.fromARGB(255, 5, 45, 97),
+                    ),
+                    new Positioned(
+                        top: Responsive.isTablet(context) ? 0 : 5,
+                        bottom: Responsive.isTablet(context) ? 10 : 5,
+                        right: Responsive.isTablet(context) ? 680 : 300,
+                        child: Container(
+                          width: Responsive.isTablet(context) ? 290 : 200,
+                          height: 390,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                color: Color.fromARGB(62, 57, 129, 237),
+                                width: Responsive.isTablet(context) ? 30 : 20,
+                              )),
+                        )),
+                    new Positioned(
+                        top: Responsive.isTablet(context) ? 0 : 5,
+                        bottom: Responsive.isTablet(context) ? 10 : 5,
+                        right: Responsive.isTablet(context) ? 600 : 250,
+                        child: Container(
+                          width: Responsive.isTablet(context) ? 430 : 300,
+                          height: 500,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.transparent,
+                              border: Border.all(
+                                color: Color.fromARGB(62, 57, 129, 237),
+                                width: Responsive.isTablet(context) ? 30 : 20,
+                              )),
+                        )),
+                    new Positioned(
+                      top: Responsive.isTablet(context) ? 70 : 50,
+                      left: Responsive.isTablet(context) ? 150 : 50,
+                      child: Container(
+                        width: Responsive.isTablet(context) ? 540 : 267,
+                        // height: Responsive.isTablet(context) ? 45 : 40,
+                        // color: Colors.amber,
+                        child: Text(
+                          "พร้อมวางแผนให้ธุรกิจคุณ!",
+                          style: GoogleFonts.ibmPlexSans(
+                              color: Colors.white,
+                              fontSize: Responsive.isTablet(context) ? 36 : 24,
+                              fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ),
+                    new Positioned(
+                        top: Responsive.isTablet(context) ? 170 : 100,
+                        left: Responsive.isTablet(context) ? 150 : 50,
+                        child: Text(
+                          Responsive.isTablet(context)
+                              ? "ให้องค์กรของคุณ วางแผนและจัดการกับ DATA\nหัวใจสำคัญของธุรกิจ ได้ถูกต้องตามกฎหมาย ปรึกษาเรา\n#TeamWiseWork"
+                              : "ให้องค์กรของคุณ วางแผนและจัดการกับ DATA\nหัวใจสำคัญของธุรกิจ ได้ถูกต้องตามกฎหมาย\nปรึกษาเรา #TeamWiseWork",
+                          style: GoogleFonts.ibmPlexSansThai(
+                              fontSize: Responsive.isTablet(context) ? 20 : 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white),
+                          textAlign: TextAlign.start,
+                        )),
+                    new Positioned(
+                      top: Responsive.isTablet(context) ? 300 : 200,
+                      left: Responsive.isTablet(context) ? 280 : 100,
+                      child: SizedBox(
+                        width: 193,
+                        height: 46,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 75, 195, 211),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                          ),
+                          child: Text(
+                            'ติดต่อเรา',
+                            style: GoogleFonts.ibmPlexSansThai(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-            ),
-          )
-        ],
-      ),
-    ));
+              ));
   }
 }
