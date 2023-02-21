822624c183241c506c67c3493d90a9c072747bc2
kukkook <lukkadeis22@gmail.com>
2023-02-21 23:34:49 +0700

    customer zone eiei

--- lib/customers/advise.dart
+++ lib/customers/advise.dart
@@ -1,31 +1,127 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wiseintern/responsive.dart';
+import 'package:wiseintern/model/customer_carousel.dart';
 
 class Advise extends StatefulWidget {
   @override
   _Advise createState() => _Advise();
 }
 
+class RealStory {
+  final String img;
+  final String brand;
+  final String story;
+  RealStory(this.img, this.brand, this.story);
+}
+
 class _Advise extends State<Advise> {
+  List<RealStory> stories = [
+    RealStory("customer/bitkub.png", "Bitkub Online",
+        "บริษัท บิทคับ แคปปิตอล กรุ๊ป โฮลดิ้งส์ จำกัด\nให้บริการเกี่ยวกับเทคโนโลยีบล็อกเชน"),
+    RealStory("customer/lotus.png", "Lotus Mattress",
+        "โลตัส แมทเทรส บริษัทเครื่องนอนชั้นนำคุณภาพสูง\nรายใหญ่ระดับโลก ก่อตั้งตั้งแต่ปี 1980 "),
+    RealStory("customer/viriya.png", "วิริยะประกันภัย จำกัด (มหาชน)",
+        "บริษัทประกันวินาศภัยอันดับหนึ่งของไทย\nครองใจยาวนานกว่า 69 ปี"),
+    RealStory("customer/tvo.png", "Thai Vegetable Oil",
+        "ผู้ประกอบการอุตสาหกรรมเกษตร จัดจำหน่ายสินค้า\nทั้งตลาดในประเทศ และต่างประเทศ"),
+    RealStory("customer/brr.png", "น้ำตาลบุรีรัมย์ จำกัด (มหาชน) ",
+        "โรงงานน้ำตาลบุรีรัมย์ เป็นหนึ่งในบรรดาผู้บุกเบิก\nอุตสาหกรรมน้ำตาลของภาคตะวันออกเฉียงเหนือ"),
+    RealStory("customer/kyocera.png", "Kyocera (Thailand)",
+        "ผู้ผลิตชั้นนำระดับสากล\nด้านอุปกรณ์อิเล็กทรอนิกส์จากประเทศญี่ปุ่น"),
+    RealStory("customer/advice.png", "Advice IT Infinite",
+        "ผู้นำด้านการจัดจำหน่ายสินค้าไอที สมาร์ทโฟน\nจำหน่ายทั่วประเทศไทย และสปป.ลาว กว่า 333 สาขา"),
+    RealStory("customer/p-pat.png", "โรงพยาบาล ป.แพทย์",
+        "บริษัท บิทคับ แคปปิตอล กรุ๊ป โฮลดิ้งส์ จำกัด\nให้บริการเกี่ยวกับเทคโนโลยีบล็อกเชน "),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: [
+          Container(
+            width: 1440,
+            color: Color.fromARGB(255, 232, 242, 254),
+            child: Padding(
+              padding: const EdgeInsets.all(60),
+              child: Container(
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 20,
+                        mainAxisSpacing: 20,
+                        childAspectRatio: 405 / 390),
+                    itemCount: stories.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        child: new Material(
+                          child: new Container(
+                            width: 405,
+                            height: 390,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Container(
+                                // margin: EdgeInsets.all(15),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 48, left: 6, right: 6, bottom: 12),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      SizedBox(
+                                          width: 150,
+                                          height: 150,
+                                          child: Image.asset(
+                                            '${stories[index].img}',
+                                            // fit: BoxFit.fill,
+                                          )),
+                                      Text(
+                                        '${stories[index].brand}',
+                                        style: GoogleFonts.ibmPlexSansThai(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 30,
+                                            color: Color.fromARGB(
+                                                255, 33, 31, 37)),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      Text(
+                                        '${stories[index].story}',
+                                        style: GoogleFonts.ibmPlexSansThai(
+                                          fontWeight: FontWeight.w400,
+                                          color:
+                                              Color.fromARGB(255, 61, 58, 65),
+                                          fontSize: 16,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 51),
             child: Container(
-              height: 800,
               width: 1440,
+              height: 750,
               // color: Colors.green,
               child: Row(
                 children: [
                   Column(
                     children: [
                       Padding(
-                        padding:
-                            const EdgeInsets.only(top: 167.39, left: 124.74),
+                        padding: const EdgeInsets.only(top: 150, left: 124.74),
                         child: Container(
                           height: 600,
                           width: 583.78,
@@ -131,6 +227,7 @@ class _Advise extends State<Advise> {
               ),
             ),
           ),
+          CustomerCarousel(),
           Container(
             height: 206,
             width: 1440,
